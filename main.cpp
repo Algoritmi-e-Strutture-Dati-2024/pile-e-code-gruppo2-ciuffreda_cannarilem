b11f11b76c80f6a230b4734938c3875e069fd106
ludovicoaciuffreda <l.ciuffreda4@studenti.uniba.it>
2024-12-06 11:43:32 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ int main() {
         pila.push(43);
         pila.push(12);
 
-        std::cout << "Top della pila: " << pila.top() << std::endl; // Dovrebbe essere 30
-        std::cout << "Dimensione della pila: " << pila.size() << std::endl; // Dovrebbe essere 3
+        std::cout << "Top della pila: " << pila.top() << std::endl; 
+        std::cout << "Dimensione della pila: " << pila.size() << std::endl; 
 
-        std::cout << "Pop dalla pila: " << pila.pop() << std::endl; // Dovrebbe rimuovere 30
-        std::cout << "Top della pila dopo pop: " << pila.top() << std::endl; // Dovrebbe essere 20
-        std::cout << "Dimensione della pila dopo pop: " << pila.size() << std::endl; // Dovrebbe essere 2
+        std::cout << "Pop dalla pila: " << pila.pop() << std::endl; 
+        std::cout << "Top della pila dopo pop: " << pila.top() << std::endl; 
+        std::cout << "Dimensione della pila dopo pop: " << pila.size() << std::endl; 
     
    
     // Test della classe Coda
@@ -29,12 +29,12 @@ int main() {
         coda.enqueue(260);
         coda.enqueue(91);
 
-        std::cout << "Front della coda: " << coda.front() << std::endl; // Dovrebbe essere 10
-        std::cout << "Dimensione della coda: " << coda.getSize() << std::endl; // Dovrebbe essere 3
+        std::cout << "Front della coda: " << coda.front() << std::endl; 
+        std::cout << "Dimensione della coda: " << coda.getSize() << std::endl; 
 
-        std::cout << "Dequeue dalla coda: " << coda.dequeue() << std::endl; // Dovrebbe rimuovere 10
-        std::cout << "Front della coda dopo dequeue: " << coda.front() << std::endl; // Dovrebbe essere 20
-        std::cout << "Dimensione della coda dopo dequeue: " << coda.getSize() << std::endl; // Dovrebbe essere 2
+        std::cout << "Dequeue dalla coda: " << coda.dequeue() << std::endl; 
+        std::cout << "Front della coda dopo dequeue: " << coda.front() << std::endl; 
+        std::cout << "Dimensione della coda dopo dequeue: " << coda.getSize() << std::endl; 
     
 
     return 0;
